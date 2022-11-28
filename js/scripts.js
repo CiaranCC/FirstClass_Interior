7ab0be8d0b0cddd49f402143719050022001de4f
CiaranCC <116034632+CiaranCC@users.noreply.github.com>
2022-11-28 22:28:43 +1300

    input code for CEO/MD change for about us html
    I have created code for the about us html section covering the CEO and MD. This code will swap the ceo with the md with a push of a button and if the user wants to go back to the ceo all they have to do is press the button again

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,38 @@ function onClick() {
 var button1 = document.getElementById('button1');
 button1.style.visibility = "hidden";
 
+function changeMD()
+{
+   /* Find all the elements*/
+
+   var CEO_heading = document.getElementById('CEO_HD');
+   var CEO_des = document.getElementById('CEO_DES');
+   var CEO_img = document.getElementById('CEOimg');
+   var CEO_btn = document.getElementById('CEO_BTN');
+
+
+   if(CEO_heading.textContent == "CEO")
+   {
+        CEO_heading.innerHTML = "MD";
+        CEO_heading.style.color = "blue";
+        CEO_heading.style.fontSize = "50px";
+        CEO_des.innerHTML = "Inspecting and updating patients' charts. Diagnosing common ailments such as colds, flu, and diabetes. Administering medication topically, orally, and via intravenous or intramuscular injections. Prescribing apt medications and lifestyle alterations..";
+       /* CEO_img.src = "https://images.squarespace-cdn.com/content/53b599ebe4b08a2784696956/1482287990797-VY0DDX7AE1UKLY3OZGXV/Headshots-NYC-CEOportrait-027.jpg?format=1000w&content-type=image%2Fjpeg"*/
+        CEO_img.src = "../images/md.webp";
+        CEO_btn.innerHTML = "Change to CEO";
+   }
+   else
+   {
+        CEO_heading.innerHTML = "CEO";
+        CEO_heading.style.color = "blue";
+        CEO_heading.style.fontSize = "50px";
+        CEO_des.innerHTML = "Making major corporate decisions, managing the overall operations and resources of a company. Communicating, on behalf of the company, with shareholders, government entities, and the public. Maintaining awareness of the competitive market landscape, expansion opportunities, industry developments, etc.";
+        CEO_img.src = "../images/CEO.jpg";
+        CEO_btn.innerHTML = "Change to MD"
+   }
+}
+
+
 function hideStaff1(){
   var staff = document.getElementById('staff1');
   staff.style.visibility = "hidden";
