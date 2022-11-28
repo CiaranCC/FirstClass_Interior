f990d05d28278854bf78b419c39c6de874873938
CiaranCC <116034632+CiaranCC@users.noreply.github.com>
2022-11-28 21:40:27 +1300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,118 @@
+
+var clicks = 0;
+
+function onClick() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+};
+var button1 = document.getElementById('button1');
+button1.style.visibility = "hidden";
+
+function hideStaff1(){
+  var staff = document.getElementById('staff1');
+  staff.style.visibility = "hidden";
+}
+
+function showButton1(){
+  button1.style.visibility = "visible";
+}
+
+function hideButton1(){
+  button1.style.visibility = "hidden";
+}
+
+function hideStaff2(){
+  var staff = document.getElementById('staff2');
+  staff.style.visibility = "hidden";
+}
+
+function showButton2(){
+  button2.style.visibility = "visible";
+}
+
+function hideButton2(){
+  button2.style.visibility = "hidden";
+}
+
+function hideStaff3(){
+  var staff = document.getElementById('staff3');
+  staff.style.visibility = "hidden";
+}
+
+function showButton3(){
+  button3.style.visibility = "visible";
+}
+
+function hideButton3(){
+  button3.style.visibility = "hidden";
+}
+
+function hideStaff4(){
+  var staff = document.getElementById('staff4');
+  staff.style.visibility = "hidden";
+}
+
+function showButton4(){
+  button4.style.visibility = "visible";
+}
+
+function hideButton4(){
+  button4.style.visibility = "hidden";
+}
+
+function hideStaff5(){
+  var staff = document.getElementById('staff5');
+  staff.style.visibility = "hidden";
+}
+
+function showButton5(){
+  button5.style.visibility = "visible";
+}
+
+function hideButton5(){
+  button5.style.visibility = "hidden";
+}
+
+function hideStaff6(){
+  var staff = document.getElementById('staff6');
+  staff.style.visibility = "hidden";
+}
+
+function showButton6(){
+  button6.style.visibility = "visible";
+}
+
+function hideButton6(){
+  button6.style.visibility = "hidden";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function headingchange(){
+
+    var Change_heading = document.getElementById("HEADING");
+
+    Change_heading.style.fontSize = "100px";
+
+    Change_heading.style.color = "#6495ED";
+
+  }
+  function align() {
+    document.getElementById("align").style.textAlign = "right";
+    document.getElementById("align1").style.textAlign = "right";
+    document.getElementById("align2").style.textAlign = "right";
+    document.getElementById("align3").style.textAlign = "right";
+    document.getElementById("align4").style.textAlign = "right";
+  }
